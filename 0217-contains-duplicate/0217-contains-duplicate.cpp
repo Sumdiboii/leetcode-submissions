e2512dd5eb8d0b601494ac67da5fe3ac8108c3c7
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2025-08-05 19:47:45 +0530

    Time: 54 ms (41.86%), Space: 90.8 MB (46.66%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hset;
+
+        for (int num : nums) {
+            if (hset.count(num)) {
+                return true;
+            } else {
+                hset.insert(num);
+            }
+        }
+        return false; 
+    }
+};
