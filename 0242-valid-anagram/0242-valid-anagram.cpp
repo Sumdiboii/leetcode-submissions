ad71315a73557df6a922b3421158159a23015215
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2025-08-18 01:39:02 +0530

    Time: 7 ms (20.47%), Space: 9.6 MB (73.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,20 +6,17 @@ public:
             return false;
         }
         else{
-            unordered_map<char,int> Scount;
-            unordered_map<char,int> Tcount;
+           sort(s.begin(), s.end());
+           sort(t.begin(),t.end());
 
-            for(int i=0;i<s.length();i++){
-                Scount[s[i]]++;
-                Tcount[t[i]]++;
-            }
+           if(s==t){
+            return true;
 
-            if(Scount == Tcount){
-                return true;
+           }
+           else{
+            return false;
+           }
             }
-            else{
-                return false;
-            }
-        }
+        
     }
 };
