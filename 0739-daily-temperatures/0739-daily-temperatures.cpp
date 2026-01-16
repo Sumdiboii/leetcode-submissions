d4af70c8eaf6c5624b88d497bf99e644262d94ee
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-01-16 17:59:52 +0530

    Time: 15 ms (85.44%), Space: 103 MB (71.16%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,15 +3,20 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         vector<int> res(n, 0);
-        stack<int> st; // stores indices
 
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
-                int idx = st.top();
-                st.pop();
-                res[idx] = i - idx;
+        for (int i = n - 2; i >= 0; i--) {
+            int j = i + 1;
+            while (j < n && temperatures[j] <= temperatures[i]) {
+                if (res[j] == 0) {
+                    j = n;
+                    break;
+                }
+                j += res[j];
             }
-            st.push(i);
+
+            if (j < n) {
+                res[i] = j - i;
+            }
         }
         return res;
     }
