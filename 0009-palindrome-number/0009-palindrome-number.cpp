7a1957f17a3953d4426fe909a9eb9e8ec31883e3
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-01-19 16:10:29 +0530

    Time: 3 ms (46.3%), Space: 10.9 MB (9.06%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,22 +3,14 @@ public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
 
-        vector<int> digits;
-        int temp = x;
+        string s = to_string(x);
+        int l = 0, r = s.length() - 1;
 
-        while (temp > 0) {
-            digits.push_back(temp % 10);
-            temp /= 10;
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
         }
-
-        int left = 0, right = digits.size() - 1;
-
-        while (left < right) {
-            if (digits[left] != digits[right]) return false;
-            left++;
-            right--;
-        }
-
         return true;
     }
 };
