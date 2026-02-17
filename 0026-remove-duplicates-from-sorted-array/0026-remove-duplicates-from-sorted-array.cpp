6677e9420ddf18acd82300cf776698b4277ac322
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-02-18 00:25:01 +0530

    Time: 0 ms (100%), Space: 22.7 MB (52.13%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        
-        int l = 0; // points to last unique element
-        for (int r = 1; r < nums.size(); r++) {
-            if (nums[r] != nums[l]) {
+          if(nums.empty()){
+            return 0;
+          }
+
+          int l=1;
+          
+
+          for(int r=1;r<nums.size();r++){
+            if(nums[r]!=nums[r-1]){
+                nums[l]= nums[r];
                 l++;
-                nums[l] = nums[r]; // move unique element forward
             }
-        }
-        return l + 1; // length of unique elements
+          }
+          return l;
     }
 };
