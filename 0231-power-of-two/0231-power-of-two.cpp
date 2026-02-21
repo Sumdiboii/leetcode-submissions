e0291d86fb6fedb2be0d4cb6fe54acdd24bca096
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-02-22 01:19:09 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.45%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        if (n <= 0) return false;          // negative numbers and 0 are never powers of two
+        return (n & (n - 1)) == 0;         // classic bit trick
     }
 };
