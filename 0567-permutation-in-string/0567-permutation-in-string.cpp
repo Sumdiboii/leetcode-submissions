f29aa53308a68a37dd7836bf8644cba6389da324
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-02-23 12:57:08 +0530

    Time: 5 ms (47.95%), Space: 10 MB (45.34%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.length()>s2.length()){
-            return false;
+        int n = s1.length();
+        int m = s2.length();
+
+        if(n>m){
+            return 0;
+        }
+        vector<int> s1vect(26, 0);
+        vector<int> s2vect(26, 0);
+
+        for (char ch : s1) {
+            s1vect[ch - 'a']++;
         }
 
-        vector<int> s1count(26,0);
-        vector<int> s2count(26,0);
-        
+        for( int i =0; i<n; i++){
+            s2vect[s2[i] - 'a']++;
 
-         int n = s2.length();
-         int m = s1.length();
-
-         for(char ch : s1){
-            s1count[ ch - 'a']++;
-         }
-
-         for( int i = 0; i < m; i++){
-            s2count[ s2[i] - 'a']++;
-
-            if(s1count == s2count){
-                return true;
-            } 
-            else{
-                continue;
-            }
-         }
-
-         for(int i = m; i < n; i++){
-            s2count[ s2[i] - 'a']++;
-            s2count[ s2[i - m] - 'a']--;
-
-            if(s1count==s2count){
+            if(s1vect==s2vect){
                 return true;
             }
-         }
-         return false;
+        }
 
+
+         for( int i =n; i<m; i++){
+            s2vect[s2[i] - 'a']++;
+            s2vect[s2[i-n] - 'a']--;
+
+            if(s1vect==s2vect){
+                return true;
+            }
+        }
+        return false;
     }
 };
