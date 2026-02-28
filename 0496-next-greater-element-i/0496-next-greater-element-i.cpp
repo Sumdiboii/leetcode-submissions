5e7d44564359592b2bb854c4f3bb8ecd6f1b652a
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-02-28 13:05:12 +0530

    Time: 3 ms (41.77%), Space: 12.8 MB (58.71%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    
-    int n = nums2.size();
-    vector<int> ans(n, -1);
-    stack<int> st;   // stores indices
-    
-    // ===== TEMPLATE START =====
-    for(int i = 0; i < n; i++) {
         
-        while(!st.empty() && nums2[st.top()] < nums2[i]) {
-            ans[st.top()] = nums2[i];
-            st.pop();
+        int n = nums2.size();
+        int m = nums1.size();
+
+        stack<int> monostack;
+        vector<int> ans(n, -1);
+        unordered_map<int, int> umap;
+
+        for (int i = 0; i < n; i++) {
+
+            while (!monostack.empty() && nums2[monostack.top()] < nums2[i]) {
+                ans[monostack.top()] = nums2[i];
+                monostack.pop();
+            }
+
+            monostack.push(i);
         }
-        
-        st.push(i);
+
+        for (int i = 0; i < n; i++) {
+            umap[nums2[i]] = ans[i];
+        }
+
+        vector<int> result;
+
+        for (int i = 0; i < m; i++) {
+            result.push_back(umap[nums1[i]]);
+        }
+
+        return result;
     }
-    // ===== TEMPLATE END =====
-    
-    // Step 2: Map values -> next greater
-    unordered_map<int,int> mp;
-    for(int i = 0; i < n; i++) {
-        mp[nums2[i]] = ans[i];
-    }
-    
-    // Step 3: Answer nums1 queries
-    vector<int> result;
-    for(int num : nums1) {
-        result.push_back(mp[num]);
-    }
-    
-    return result;
-}
 };
