f8ab300f305568355041e6755abcaba4ef715e55
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-01 12:44:00 +0530

    Time: 0 ms (100%), Space: 12.2 MB (96.3%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-int i=m;
-        for(int j=0;  j<n; j++){
-            nums1[i]=nums2[j];
-            i++;
+        for(int i = m; i < m + n; i++){
+            nums1[i] = nums2[i - m];
         }
 
         sort(nums1.begin(), nums1.end());
-        
     }
 };
