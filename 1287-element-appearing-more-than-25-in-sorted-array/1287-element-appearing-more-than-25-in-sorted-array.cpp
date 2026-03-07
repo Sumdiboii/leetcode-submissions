aa1ba36d27a4f53423520bb900935f1c86607876
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-07 08:49:57 +0530

    Time: 4 ms (25.35%), Space: 18 MB (21.04%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
         int n = arr.size();
-        int t = n / 4; // threshold
+        if(n == 1) return arr[0];
 
-        int count = 1;
-        for(int i = 1; i < n; i++) {
-            if(arr[i] == arr[i-1]) {
-                count++;
-                if(count > t) return arr[i];
-            } else {
-                count = 1;
-            }
+        int t = n/4; // threshold
+
+        unordered_map<int,int> freq;
+        for(int x : arr) freq[x]++;
+
+        // iterate through array in order to respect first occurrence
+        for(int x : arr) {
+            if(freq[x] > t) return x;
         }
 
-        return arr[0]; // first element covers >25% if array size is 1 or extreme case
+        return -1; // safety, should never happen
     }
 };
