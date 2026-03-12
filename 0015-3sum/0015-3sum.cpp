67b8b30c3dca3e2b151dff9baba53abc04393b0b
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-13 00:00:15 +0530

    Time: 63 ms (18.37%), Space: 29.2 MB (45.58%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+        int n = nums.size();
+
         sort(nums.begin(), nums.end());
-        unordered_map<int, int> count;
-        for (int num : nums) {
-            count[num]++;
-        }
 
-        vector<vector<int>> res;
-        for (int i = 0; i < nums.size(); i++) {
-            count[nums[i]]--;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for(int i = 0; i < n; i++) {
 
-            for (int j = i + 1; j < nums.size(); j++) {
-                count[nums[j]]--;
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
 
-                int target = -(nums[i] + nums[j]);
-                if (count[target] > 0) {
-                    res.push_back({nums[i], nums[j], target});
+            int l = i + 1;
+            int r = n - 1;
+
+            while(l < r) {
+
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if(sum == 0) {
+
+                    ans.push_back({nums[i], nums[l], nums[r]});
+
+                    while(l < r && nums[l] == nums[l+1]) l++;
+                    while(l < r && nums[r] == nums[r-1]) r--;
+
+                    l++;
+                    r--;
                 }
-            }
 
-            for (int j = i + 1; j < nums.size(); j++) {
-                count[nums[j]]++;
+                else if(sum < 0)
+                    l++;
+
+                else
+                    r--;
             }
         }
 
-        return res;
+        return ans;
     }
 };
