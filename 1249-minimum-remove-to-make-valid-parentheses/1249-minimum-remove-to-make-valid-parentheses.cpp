5f7002337bc8e10c5d95130be7a0c3e7b8ef0e90
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-16 19:57:27 +0530

    Time: 9 ms (51.81%), Space: 14.8 MB (20.77%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+
+        string temp;
+        int open = 0;
+
+        // First pass: remove invalid ')'
+        for(char c : s) {
+            if(c == '(') {
+                open++;
+                temp.push_back(c);
+            }
+            else if(c == ')') {
+                if(open > 0) {
+                    open--;
+                    temp.push_back(c);
+                }
+            }
+            else {
+                temp.push_back(c);
+            }
+        }
+
+        string ans;
+
+        // Second pass: remove extra '('
+        for(int i = temp.size() - 1; i >= 0; i--) {
+            if(temp[i] == '(' && open > 0) {
+                open--;
+                continue;
+            }
+            ans.push_back(temp[i]);
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
