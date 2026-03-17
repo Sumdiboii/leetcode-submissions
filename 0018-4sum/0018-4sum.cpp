7981c595235f14c75d1bea2d90c7e53f411a2555
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-17 12:42:26 +0530

    Time: 25 ms (29.93%), Space: 17.6 MB (13.34%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -11,34 +11,38 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        
+
         for (int i = 0; i < n; i++) {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            for (int j = i + 1; j < n;) {
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
                 int start = j + 1, end = n - 1;
 
                 while (start < end) {
-                    long long sum = (long long)nums[i] + (long long)nums[j] +
-                                    (long long)nums[start] +
-                                    (long long)nums[end];
-                    if (target == sum) {
-                        ans.push_back(
-                            {nums[i], nums[j], nums[start], nums[end]});
+                    long long sum = (long long)nums[i] + nums[j] +
+                                    nums[start] + nums[end];
+
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[start], nums[end]});
                         start++;
                         end--;
+
                         while (start < end && nums[start] == nums[start - 1])
                             start++;
-                    } else if (sum < target)
+                        while (start < end && nums[end] == nums[end + 1])
+                            end--;
+                    }
+                    else if (sum < target) {
                         start++;
-                    else
+                    }
+                    else {
                         end--;
+                    }
                 }
-                j++;
-
-                while (j < n && nums[j] == nums[j - 1])
-                    j++;
             }
         }
         return ans;
