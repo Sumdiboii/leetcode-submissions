2e117401165d441f970be8ebabe6870c3ba4c9e2
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-20 13:34:27 +0530

    Time: 0 ms (100%), Space: 19.5 MB (64.24%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,32 +1,18 @@
-// static const auto fast_io = [](){
-//     ios :: sync_with_stdio(false);
-//     cin.tie(nullptr);
-//     cout.tie(nullptr);
-//     return 0;
-
-// }();
-
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int left=0;
-        int right=numbers.size()-1;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0;
+        int r = n - 1;
 
-        while(left<right){
-            if (numbers[left]+numbers[right]<target){
-                left++;
-                continue;
-            }
-
-            if (numbers[left]+numbers[right]>target){
-                right--;
-                continue;
-            }
-
-            if(numbers[left]+ numbers[right]==target){
-                return{left+1, right+1};
-            }
+        while (l < r) {
+            if (nums[l] + nums[r] == target) {
+                return {l + 1, r + 1};
+            } else if (nums[l] + nums[r] < target) {
+                l++;
+            } else
+                r--;
         }
-        return{};
+        return {};
     }
 };
