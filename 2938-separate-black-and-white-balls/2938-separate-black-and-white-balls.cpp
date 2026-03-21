54a0af35c1f892b6c0a1a25c90f456b6cc5db652
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-22 02:50:39 +0530

    Time: 4 ms (75%), Space: 17.2 MB (71.24%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     long long minimumSteps(string s) {
         long long swap = 0;
-        int black = 0; 
+        int black = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '0') 
-                swap += (long long) black; 
+            if (s[i] == '0')
+                swap += (long long)black;
             else
-                black++; 
+                black++;
         }
         return swap;
     }
