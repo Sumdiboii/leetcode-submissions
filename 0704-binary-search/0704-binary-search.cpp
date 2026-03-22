8b20d39f80aee0247a8e7d0e99a3af2be77b2681
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-22 15:36:13 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.79%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        int n  = nums.size();
 
-        int l = 0;
-        int r = nums.size() - 1;
+        int l =0; 
+        int r = n-1;
 
-        while(l <= r) {                // <= to include last element
-            int mid = l + (r - l) / 2; // avoid overflow
+        while(l<=r){
+            int m = l+ (r-l) /2;
 
-            if(nums[mid] == target) {
-                return mid;             // check first
+            if( nums[m] == target){
+                return m;
             }
-            else if(nums[mid] < target) {
-                l = mid + 1;
-            } 
-            else {
-                r = mid - 1;
+            else if( nums[m] < target){
+                l = m+1;
+            }
+            else{
+                r = m-1;
             }
         }
 
-        return -1;                      // target not found
+        return -1;
     }
 };
