39122e438c2209b00a0c7e0700e9eccbda1bcb08
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-25 16:52:19 +0530

    Time: 7 ms (18.38%), Space: 20.6 MB (83.04%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,17 @@
-class Solution{    
-public:    
-    int singleNumber(vector<int>& nums){
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+    int n = nums.size();
 
-        int n = nums[0];
-         for( int i  = 1 ; i< nums.size(); i++){
-            n = n ^ nums[i];
-         }
-         return n;
+    sort(nums.begin(), nums.end());
+    int res = nums[0];
+
+
+
+    for( int i = 1; i< n ; i++){
+        res = res ^ nums[i];
+    }
+return res;
+
     }
 };
