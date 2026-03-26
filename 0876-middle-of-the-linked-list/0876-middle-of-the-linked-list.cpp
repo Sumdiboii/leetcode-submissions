9062516fba12884beeb513c55e636fbce6c4c352
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-26 23:36:04 +0530

    Time: 0 ms (100%), Space: 10 MB (25.74%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,14 +11,15 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) return head;
         ListNode* slow = head;
         ListNode* fast = head;
-        
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
+
+     while(fast && fast -> next ){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+     }
+
+     return slow;
+           
     }
 };
