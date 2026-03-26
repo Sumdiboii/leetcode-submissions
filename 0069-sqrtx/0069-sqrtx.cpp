f05b4ed4c6b8d27f88e555e5a72fe5a977d1c2a0
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-03-26 13:49:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.73%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int l = 0;
+        int r = x;
+        int ans = 0;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if ((long long)mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return ans;
     }
 };
