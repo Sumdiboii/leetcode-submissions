5496caefef3cd97daa737eca471d871cc74fc61f
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-04-01 14:02:20 +0530

    Time: 0 ms (100%), Space: 27.3 MB (15.5%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,23 +1,8 @@
-#define n s.size()
-
-static const auto fast_io = [](){
-    ios:: sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-} ();
-
-
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int l =0;
-        int r = n-1;
+        reverse(s.begin(), s.end());
 
-        while(l<r) {
-            swap (s[l], s[r]);
-            l++;
-            r--;
-        }
+        
     }
 };
