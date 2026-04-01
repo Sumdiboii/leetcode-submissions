d12f9d274d62aea09310f04d91474a38b15cb7a9
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-04-01 11:32:54 +0530

    Time: 3 ms (30.42%), Space: 9.8 MB (93.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0, right = s.size() - 1;
+        int n = s.length();
+        int l = 0;
+        int r = n-1;
 
-        while (left < right) {
+        while(l<r){
+            while(l<r && !isalnum(s[l])){
+                l++;
+            } 
+            while(l<r && !isalnum(s[r])){
+                r--;
+            } 
 
-            // skip non-alphanumeric (left)
-            while (left < right && !isalnum(s[left])) left++;
-
-            // skip non-alphanumeric (right)
-            while (left < right && !isalnum(s[right])) right--;
-
-            // compare lowercase
-            if (tolower(s[left]) != tolower(s[right])) {
+            if(tolower(s[l]) != tolower(s[r])){
                 return false;
             }
-
-            left++;
-            right--;
+            else{
+                l++;
+                r--;
+            }
         }
 
         return true;
