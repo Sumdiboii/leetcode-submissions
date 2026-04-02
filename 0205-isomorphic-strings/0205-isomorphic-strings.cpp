6e2f031c885d791b1c878ed4fab2437c078231b3
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-04-02 18:00:29 +0530

    Time: 0 ms (100%), Space: 9.3 MB (54.46%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,26 +4,27 @@ public:
         int n = s.length();
         int m = t.length();
 
-        unordered_map<char, char> umap1;
+        unordered_map<char,char> umap1;
+        unordered_map<char,char> umap2;
         
-        unordered_map<char, char> umap2;
 
-        if(n != m){
+        if(n != m ){
             return false;
         }
 
         for( int i = 0; i< n; i++){
 
-
             if(umap1.count(s[i]) && umap1[s[i]] != t[i]){
                 return false;
             }
+
             if(umap2.count(t[i]) && umap2[t[i]] != s[i]){
                 return false;
             }
             
             umap1[s[i]] = t[i];
             umap2[t[i]] = s[i];
+            
 
         }
 
