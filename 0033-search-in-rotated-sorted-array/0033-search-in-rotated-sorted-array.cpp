7a295180275e04a768f2987fb0e18f197b4fb6cc
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-04-24 03:50:20 +0530

    Time: 0 ms (100%), Space: 15.1 MB (95.83%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0;
-        int r = n - 1;
+        int l = 0, r = nums.size() - 1;
 
         while (l <= r) {
             int mid = l + (r - l) / 2;
 
-            if (nums[mid] == target) {
-                return mid;
-            }
+            if (nums[mid] == target) return mid;
 
-            // Left half is sorted
+            // Check if left half is sorted
             if (nums[l] <= nums[mid]) {
                 if (nums[l] <= target && target < nums[mid]) {
-                    r = mid - 1;
+                    r = mid - 1; // Target is in the left half
                 } else {
-                    l = mid + 1;
+                    l = mid + 1; // Target is in the right half
                 }
-            }
-            // Right half is sorted
+            } 
+            // Otherwise, the right half must be sorted
             else {
                 if (nums[mid] < target && target <= nums[r]) {
-                    l = mid + 1;
+                    l = mid + 1; // Target is in the right half
                 } else {
-                    r = mid - 1;
+                    r = mid - 1; // Target is in the left half
                 }
             }
         }
-
         return -1;
     }
 };
