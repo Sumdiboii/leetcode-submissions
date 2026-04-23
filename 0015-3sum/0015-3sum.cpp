cad1a4c97eb7144906cd9f2410c3ed045ce97f50
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-04-24 03:44:36 +0530

    Time: 55 ms (28.22%), Space: 29.1 MB (71.98%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,31 +6,32 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        for ( int i = 0; i< n; i++){
+        for( int i = 0; i< n; i++){
             if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
 
 
-            int l = i+1; 
+            int l = i+1;
             int r = n-1;
 
             while(l<r){
-                if( nums[i] + nums[l] + nums[r] == 0){
+                if(nums[i] + nums[l] + nums[r] == 0){
                     ans.push_back({nums[i], nums[l], nums[r]});
 
-                    while( l<r && nums[l] == nums[l+1]){
+                    while(l<r &&  nums[l] == nums[l+1]){
                         l++;
                     }
-                    while( l<r && nums[r] == nums[r-1]){
+
+                     while(l<r &&  nums[r] == nums[r-1]){
                         r--;
                     }
 
+
                     l++;
                     r--;
-
-
                 }
+
                 else if(nums[i] + nums[l] + nums[r] > 0){
                     r--;
                 }
