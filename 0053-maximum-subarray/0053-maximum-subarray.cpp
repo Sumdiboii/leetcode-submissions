988c18f0ca371203ceb243e2fe927823c83fda7f
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-04-24 02:56:46 +0530

    Time: 0 ms (100%), Space: 71.8 MB (15.04%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN;
-        int currentSum = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            currentSum += nums[i];
-            
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
+        long long maxsum = INT_MIN;
+        long long curr = 0;
+
+
+        for ( int i = 0; i< nums.size(); i++){
+            curr += nums[i];
+
+            if( curr> maxsum){
+                maxsum = curr;
             }
-            
-            if (currentSum < 0) {
-                currentSum = 0;
+
+
+            if( curr< 0){
+                curr = 0;
             }
         }
-        
-        return maxSum;
+
+
+        return maxsum;
     }
 };
