16d99f16f7d803d505aa0b905a14215c5b6f0181
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-04-26 16:43:17 +0530

    Time: 0 ms (100%), Space: 10.7 MB (57.28%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,12 @@
+#include <vector>
+using namespace std;
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> results;
-        int cnt=0;
-
-        for( int i = 0; i<=n; i++){
-            cnt = __builtin_popcount(i);
-            results.push_back(cnt);
+        vector<int> ans(n + 1);
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
-
-        return results;
+        return ans;
     }
 };
