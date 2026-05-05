741b36025048605942a5a26c2bbd9ef27ea8246e
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-05-05 23:11:07 +0530

    Time: 0 ms (100%), Space: 8.3 MB (14.27%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    int reverseBits(int n) {
-        if(n % 2 !=0){
-            return 0;
-        }
-         uint32_t res = 0;
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
         for (int i = 0; i < 32; i++) {
             res <<= 1;
             res |= (n & 1);
             n >>= 1;
         }
         return res;
-
     }
-};
+};  
