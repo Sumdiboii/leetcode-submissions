1fc11c68ffb14767bc7624fa89969b9cb7ccad93
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-05-26 19:28:25 +0530

    Time: 0 ms (100%), Space: 8 MB (22.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -6,16 +6,20 @@ public:
             return n;
         }
 
-        vector <int> dp(n+1);
+        int prev1 = 0;
+        int prev2 = 1;
+        int res = 0;
 
-        dp[0] = 0;
-        dp[1] = 1;
 
-        for( int i = 2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        for( int i = 2; i<=n ; i++){
+            res = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = res;
         }
 
-        return dp[n];
+
+        return res;
+
 
     }
 };
