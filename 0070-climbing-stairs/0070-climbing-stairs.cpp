03bdd01f643cc4ea45174430b62d31de0a371f7a
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-05-26 16:24:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (46.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int climbStairs(int n) {
 
-        if (n <= 2) return n;
-        vector<int> dp(n+1);
+        // if (n <= 2) return n;
+
+        vector<int> dp(n+2);
 
         dp[0] = 1; dp[1] = 1;
         dp[2] = 2;
