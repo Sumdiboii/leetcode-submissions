13223d52ba78ad918dae3d7086710beb85685394
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-06-03 17:03:58 +0530

    Time: 0 ms (100%), Space: 8.4 MB (47.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& memo) {
-        // Base Cases: 1 way for step 1, 2 ways for step 2
-        if (n <= 2) return n;
-        
-        // Check the notepad: If we already calculated step n, return it instantly
-        if (memo[n] != -1) return memo[n];
-        
-        // Otherwise, calculate it recursively and save the result in the notepad
-        memo[n] = solve(n - 1, memo) + solve(n - 2, memo);
-        
-        return memo[n];
-    }
-
     int climbStairs(int n) {
-        // Create a memoization array filled with -1 (meaning "uncalculated")
-        vector<int> memo(n + 1, -1);
-        return solve(n, memo);
+        vector< int> dp(n+2, 0);
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+
+        for( int i = 3; i<= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        } 
+
+        return dp[n];
     }
 };
