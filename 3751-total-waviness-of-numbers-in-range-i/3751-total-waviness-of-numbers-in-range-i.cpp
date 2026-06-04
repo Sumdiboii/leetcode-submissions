f45a7dd4f737036cf2285c54ac2ebf63db832edb
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-06-04 08:56:50 +0530

    Time: 3 ms (96.08%), Space: 8.8 MB (92.54%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-    int count(string s){
-        int n = s.size();
-        int i = 0;
-        int cnt = 0;
-        while(i<n){
-            if(i-1>=0 && i+1<n && s[i]>s[i-1] && s[i]>s[i+1]){
-                cnt++;
-            }
-            if(i-1>=0 && i+1<n && s[i]<s[i-1] && s[i]<s[i+1]){
-                cnt++;
-            }
-            i++;
-        }
-        return cnt;
-    }
     int totalWaviness(int num1, int num2) {
-        int sum = 0;
-        for(int i = num1;i<=num2;i++){
-            sum += count(to_string(i));
+        int total_waviness = 0;
+        
+        for (int num = num1; num <= num2; ++num) {
+            // A number needs at least 3 digits to have a peak or valley
+            if (num < 100) {
+                continue; 
+            }
+                
+            int temp = num;
+            
+            // Extract the first two digits from the right
+            int right_digit = temp % 10;
+            temp /= 10;
+            int mid_digit = temp % 10;
+            temp /= 10;
+            
+            // Slide the window through the rest of the number
+            while (temp > 0) {
+                int left_digit = temp % 10;
+                
+                // Check for Peak: left < mid > right
+                // Check for Valley: left > mid < right
+                if ((left_digit < mid_digit && mid_digit > right_digit) || 
+                    (left_digit > mid_digit && mid_digit < right_digit)) {
+                    total_waviness++;
+                }
+                    
+                // Shift the window to the left
+                right_digit = mid_digit;
+                mid_digit = left_digit;
+                temp /= 10;
+            }
         }
-        return sum;
+        
+        return total_waviness;
     }
 };
