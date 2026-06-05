c81e7929411e25ba6aac4d087f5df164d2662768
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-06-05 14:08:15 +0530

    Time: 0 ms (100%), Space: 15.4 MB (99.54%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -1,30 +1,38 @@
 /**
  * Definition for singly-linked list.
  * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) return head;
-
+        // Edge case: if the list has 0, 1, or 2 nodes, no reordering is needed
+        if (head == nullptr || head->next == nullptr || head->next->next == nullptr) {
+            return head;
+        }
+        
         ListNode* odd = head;
         ListNode* even = head->next;
-        ListNode* evenHead = even;
-
+        ListNode* evenHead = even; // Remember the start of the even list
+        
+        // Loop runs as long as there are nodes to process in the even list
         while (even != nullptr && even->next != nullptr) {
-            odd->next = even->next;
-            odd = odd->next;
-            even->next = even->next->next;
-            even = even->next;
+            odd->next = even->next;   // Connect current odd to next odd
+            odd = odd->next;          // Move odd pointer forward
+            
+            even->next = odd->next;   // Connect current even to next even
+            even = even->next;        // Move even pointer forward
         }
-
-        odd->next = evenHead; // Connect odd list to even list
+        
+        // Connect the end of the odd list to the head of the even list
+        odd->next = evenHead;
+        
         return head;
     }
 };
