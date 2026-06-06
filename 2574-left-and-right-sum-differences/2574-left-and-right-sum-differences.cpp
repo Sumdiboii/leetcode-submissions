4d980ff8e397bb2b09540ed1d11eefc45014920d
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-06-06 07:43:21 +0530

    Time: 0 ms (100%), Space: 15 MB (81.37%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -4,22 +4,24 @@ public:
         int n = nums.size();
         vector<int> ans(n);
         
+        int rightSum = 0;
+        int leftSum = 0;
+        
+        // Calculate the total sum of the array first
+        for (int num : nums) {
+            rightSum += num;
+        }
+        
+        // Iterate and calculate the differences on the fly
         for (int i = 0; i < n; i++) {
-            int leftSum = 0;
-            int rightSum = 0;
+            // 1. The current element is no longer on the right
+            rightSum -= nums[i]; 
             
-            // Calculate sum of elements to the left
-            for (int j = 0; j < i; j++) {
-                leftSum += nums[j];
-            }
-            
-            // Calculate sum of elements to the right
-            for (int j = i + 1; j < n; j++) {
-                rightSum += nums[j];
-            }
-            
-            // Store the absolute difference
+            // 2. Calculate the difference
             ans[i] = abs(leftSum - rightSum);
+            
+            // 3. Add the current element to leftSum for the next iteration
+            leftSum += nums[i];  
         }
         
         return ans;
