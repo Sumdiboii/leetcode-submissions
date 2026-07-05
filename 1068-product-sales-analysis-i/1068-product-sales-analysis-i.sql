a3628d83acc56169a801a636985d7fab03778d9e
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-07-05 17:59:09 +0530

    Time: 1813 ms (5.1%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select product_name, sales.year, sales.price
+select product.product_name, sales.year, sales.price
 from sales 
 inner join product
 on sales.product_id = product.product_id
