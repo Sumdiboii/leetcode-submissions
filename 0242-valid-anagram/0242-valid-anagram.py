ace6d7aa242df4ffbac2e564f01edf8f1471c3a9
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-07-14 17:36:31 +0530

    Time: 15 ms (44.37%), Space: 19.3 MB (75.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,5 @@
+"""
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -13,3 +15,25 @@ class Solution:
             seen2[ch] = seen2.get(ch, 0) + 1
         
         return seen1 == seen2
+        """
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        counts = {}
+        
+        # Increment for string s
+        for ch in s:
+            counts[ch] = counts.get(ch, 0) + 1
+            
+        # Decrement for string t
+        for ch in t:
+            # If the character isn't in counts or the count is 0,
+            # it means t has a char that s doesn't have (or too many of them)
+            if ch not in counts or counts[ch] == 0:
+                return False
+            counts[ch] -= 1
+            
+        return True
