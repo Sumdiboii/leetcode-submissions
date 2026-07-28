ad2467af76ddcc7126044bac4bfaf7e7a79e2c38
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-07-28 23:02:56 +0530

    Time: 684 ms (15.78%), Space: 0B (100%) - LeetHub

---
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -0,0 +1,11 @@
+-- 1. Daily Leads and Partners (LeetCode 1693)
+SELECT 
+    date_id, 
+    make_name, 
+    COUNT(DISTINCT lead_id) AS unique_leads, 
+    COUNT(DISTINCT partner_id) AS unique_partners
+FROM 
+    DailySales
+GROUP BY 
+    date_id, 
+    make_name;
