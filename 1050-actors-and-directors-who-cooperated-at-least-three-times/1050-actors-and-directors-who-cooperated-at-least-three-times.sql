927a0cda7acda42014eb4ffdd3ea4d1d9ca5ccd8
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-07-28 23:03:35 +0530

    Time: 523 ms (8.35%), Space: 0B (100%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,11 @@
+-- 5. Actors and Directors Who Cooperated At Least Three Times (LeetCode 1050)
+SELECT 
+    actor_id, 
+    director_id 
+FROM 
+    ActorDirector 
+GROUP BY 
+    actor_id, 
+    director_id 
+HAVING 
+    COUNT(*) >= 3;
