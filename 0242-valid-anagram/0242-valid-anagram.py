73f2967bc98f414618c46b0f260e43e4af7c1b64
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-07-28 22:29:55 +0530

    Time: 10 ms (79.41%), Space: 19.4 MB (45.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,39 +1,15 @@
-"""
-
 class Solution:
+    # 2. Valid Anagram (LeetCode 242)
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-            
-        seen1 = {}
-        seen2 = {}
-
-        for ch in s:
-            seen1[ch] = seen1.get(ch, 0) + 1
-
-        for ch in t:
-            seen2[ch] = seen2.get(ch, 0) + 1
-        
-        return seen1 == seen2
-        """
-
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+        # If lengths differ, they cannot be anagrams
         if len(s) != len(t):
             return False
         
-        counts = {}
-        
-        # Increment for string s
-        for ch in s:
-            counts[ch] = counts.get(ch, 0) + 1
+        # Count frequencies of characters for both strings
+        count_s, count_t = {}, {}
+        for i in range(len(s)):
+            count_s[s[i]] = count_s.get(s[i], 0) + 1
+            count_t[t[i]] = count_t.get(t[i], 0) + 1
             
-        # Decrement for string t
-        for ch in t:
-            # If the character isn't in counts or the count is 0,
-            # it means t has a char that s doesn't have (or too many of them)
-            if ch not in counts or counts[ch] == 0:
-                return False
-            counts[ch] -= 1
-            
-        return True
+        # Compare frequency dictionaries
+        return count_s == count_t
