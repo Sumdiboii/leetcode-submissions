27a9c52b0bd9d4498fce845dccf9f38b7473ddef
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-07-29 13:51:05 +0530

    Time: 0 ms (100%), Space: 20.6 MB (42.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,20 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        # Handle edge case where the array is empty
         if not nums:
             return 0
-        
-        # Initialize the slow pointer to track the position of the last unique element
-        slow = 0
-        
-        # Iterate with the fast pointer starting from the second element to scan the array
-        for fast in range(1, len(nums)):
-            # Check if the current element is different from the last unique element
-            if nums[fast] != nums[slow]:
-                # Increment the slow pointer to the next position
-                slow += 1
-                # Update the position with the new unique value
-                nums[slow] = nums[fast]
-                
-        # Return the count of unique elements, which is the slow pointer index plus one
-        return slow + 1
+
+        l = 1
+
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r - 1]:
+                nums[l] = nums[r]
+                l += 1
+
+        return l
