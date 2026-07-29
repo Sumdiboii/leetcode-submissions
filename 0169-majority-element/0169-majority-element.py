99f080ce18a049842f7083eba07c2bd9a38731ab
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-07-29 13:41:32 +0530

    Time: 4 ms (70.77%), Space: 21.1 MB (84.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,16 @@
 class Solution:
-    def majorityElement(self, nums: list[int]) -> int:
-        # Initialize the candidate for the majority element and a counter for its frequency
+    def majorityElement(self, nums: List[int]) -> int:
         candidate = None
-        count = 0
-        
-        # Iterate through each number in the array using Boyer-Moore Voting Algorithm
-        for num in nums:
-            # If the count drops to zero, choose the current number as the new candidate
-            if count == 0:
-                candidate = num
-            
-            # Increment the count if the current number matches the candidate, otherwise decrement it
-            count += (1 if num == candidate else -1)
-            
-        # Return the majority element that appears more than n // 2 times
+        votes = 0
+
+        for n in nums: 
+            if(votes == 0):
+                candidate = n
+
+            if(candidate == n):
+                votes += 1
+
+            else: 
+                votes -= 1
+
         return candidate
