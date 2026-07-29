62a644213ac8253797da6bcec115da01a3a6b35c
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-07-29 12:49:00 +0530

    Time: 1 ms (88.78%), Space: 20.6 MB (23.58%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,16 +1,10 @@
 class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        # Pointer to track the position where the next non-zero element should be placed
-        last_non_zero_found_at = 0
+        # 1. Filter out all non-zero elements using a list comprehension
+        non_zeroes = [x for x in nums if x != 0]
         
-        # Traverse through the entire array
-        for i in range(len(nums)):
-            # If the current element is not zero, we need to shift it forward
-            if nums[i] != 0:
-                # Swap the current non-zero element with the element at the last_non_zero_found_at pointer
-                nums[last_non_zero_found_at], nums[i] = nums[i], nums[last_non_zero_found_at]
-                # Increment the pointer to the next available position for a non-zero element
-                last_non_zero_found_at += 1
+        # 2. Count the zeroes and generate a padding array
+        zero_count = nums.count(0)
+        
+        # 3. Use slice assignment to overwrite the original memory reference in-place
+        nums[:] = non_zeroes + [0] * zero_count
