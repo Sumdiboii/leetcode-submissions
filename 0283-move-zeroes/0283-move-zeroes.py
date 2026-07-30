6d046829dc00c400004e2ea58fbb9773d532384e
Sumedh Pimplikar <SPIMPLIKAR08@GMAIL.COM>
2026-07-30 08:18:49 +0530

    Time: 3 ms (81.72%), Space: 20.5 MB (61.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,10 +1,10 @@
 class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
-        # 1. Filter out all non-zero elements using a list comprehension
-        non_zeroes = [x for x in nums if x != 0]
+        l = 0
         
-        # 2. Count the zeroes and generate a padding array
-        zero_count = nums.count(0)
-        
-        # 3. Use slice assignment to overwrite the original memory reference in-place
-        nums[:] = non_zeroes + [0] * zero_count
+
+        for r in range(len(nums)):
+            if(nums[r] != 0):
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                
